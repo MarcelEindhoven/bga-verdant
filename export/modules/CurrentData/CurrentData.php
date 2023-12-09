935accb2f876f1031bc648e21215875be7c25003
MarcelEindhoven <Marcel.Eindhoven@Gmail.com>
2023-12-09 08:59:41 +0100

    CurrentDataTest

---
+++ export/modules/CurrentData/CurrentData.php
@@ -0,0 +1,30 @@
+<?php
+namespace NieuwenhovenGames\Verdant;
+/**
+ *------
+ * Verdant implementation : © Marcel van Nieuwenhoven marcel.eindhoven@hotmail.com
+ * This code has been produced on the BGA studio platform for use on https://boardgamearena.com.
+ * See http://en.doc.boardgamearena.com/Studio for more information.
+ *
+ */
+
+class CurrentData {
+    public static function create($sql_database) : CurrentData {
+        $object = new CurrentData();
+        return $object->setDatabase($sql_database);
+    }
+
+    public function setDatabase($sql_database) : CurrentData {
+        $storage = \NieuwenhovenGames\BGA\Storage::create($sql_database);
+        $this->setStorage($storage);
+
+        return $this;
+    }
+
+    public function setStorage($storage) : CurrentData {
+        $this->storage = $storage;
+
+        return $this;
+    }
+}
+?>
---
+++ phpunit/CurrentData/CurrentDataTest.php
@@ -0,0 +1,31 @@
+<?php
+namespace NieuwenhovenGames\Verdant;
+/**
+ *------
+ * Verdant implementation unit tests : © Marcel van Nieuwenhoven marcel.eindhoven@hotmail.com
+ *
+ */
+
+include_once(__DIR__.'/../../vendor/autoload.php');
+use PHPUnit\Framework\TestCase;
+
+include_once(__DIR__.'/../../export/modules/CurrentData/CurrentData.php');
+
+include_once(__DIR__.'/../../export/modules/BGA/UpdateStorage.php');
+include_once(__DIR__.'/../../export/modules/BGA/FrameworkInterfaces/Database.php');
+
+class CurrentDataTest extends TestCase{
+    protected CurrentData $sut;
+    protected \NieuwenhovenGames\BGA\FrameworkInterfaces\Database $mock_database;
+
+    protected function setUp(): void {
+        $this->mock_database = $this->createMock(\NieuwenhovenGames\BGA\FrameworkInterfaces\Database::class);
+        $this->sut = CurrentData::create($this->mock_database);
+    }
+    public function testSetup_Items_createCardsAndShuffle() {
+        // Arrange
+        // Act
+        // Assert
+    }
+}
+?>
