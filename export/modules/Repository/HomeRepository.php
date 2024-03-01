da73bdd9627b33de747c8d55619545390aa3b694
MarcelEindhoven <Marcel.Eindhoven@Gmail.com>
2024-03-01 19:18:06 +0100

    getUpdatedCard

--- export/modules/Repository/HomeRepository.php
+++ export/modules/Repository/HomeRepository.php
@@ -17,6 +17,8 @@ require_once(__DIR__.'/../BGA/FrameworkInterfaces/Deck.php');
 class HomeRepository extends \ArrayObject {
     const KEY_PLAYER_ID = 'location';
     const KEY_LOCATION = 'location_arg';
+    const KEY_ELEMENT_ID = 'element_id';
+
     protected array $decks = [];
     protected string $player_id = '';
 
@@ -48,10 +50,24 @@ class HomeRepository extends \ArrayObject {
     /** Content array is only guaranteed to be valid after refresh  */
     public function refresh() : HomeRepository {
         foreach ($this->decks as $name => $deck) {
-            $this[$name] = $deck->getCardsInLocation($this->player_id);
+            $this[$name] = $this->getUpdatedCards($deck->getCardsInLocation($this->player_id));
         }
 
         return $this;
+    }
+    protected function getUpdatedCards($cards) {
+        $updated_cards = [];
+        foreach ($cards as $card) {
+            $updated_cards[] = $this->getUpdatedCard($card);
+        }
+        return $updated_cards;
+    }
+    protected function getUpdatedCard($card) {
+        $updated_card = $card;
+        // '4' -> player id'_04'
+        $location = +$card[HomeRepository::KEY_LOCATION];
+        $updated_card[HomeRepository::KEY_ELEMENT_ID] = $this->player_id . '_' . intdiv($location, 10) . $location % 10;
+        return $updated_card;
     }
 }
 ?>
--- phpunit/Repository/HomeRepositoryTest.php
+++ phpunit/Repository/HomeRepositoryTest.php
@@ -39,25 +39,56 @@ class HomeRepositoryTest extends TestCase{
 
     public function testSetup__MiddleLocation__ConcatenatedElementID() {
         // Arrange
+        $this->arrangeEmptyCards();
+        
+        $item = $this->getDefaultStoredCard('24');
+        $this->mock_items
+        ->expects($this->exactly(1))
+        ->method('getCardsInLocation')
+        ->with($this->player_id)
+        ->willReturn([$item]);
+        // Act
+        $decks = (array) ($this->sut->refresh());
+        // Assert
+        $expected_item = $this->getUpdatedCard($item, $this->player_id . '_24');
+        $expected_decks = [Constants::PLANT_NAME => [], Constants::ITEM_NAME => [$expected_item], Constants::ROOM_NAME => []];
+        $this->assertEqualsCanonicalizing($expected_decks, $decks);
+    }
+
+    public function testSetup__TopLocation__UpdatedElementID() {
+        // Arrange
+        $this->arrangeEmptyCards();
+        
+        $item = $this->getDefaultStoredCard('4');
+        $this->mock_items
+        ->expects($this->exactly(1))
+        ->method('getCardsInLocation')
+        ->with($this->player_id)
+        ->willReturn([$item]);
+        // Act
+        $decks = (array) ($this->sut->refresh());
+        // Assert
+        $expected_item = $this->getUpdatedCard($item, $this->player_id . '_04');
+        $expected_decks = [Constants::PLANT_NAME => [], Constants::ITEM_NAME => [$expected_item], Constants::ROOM_NAME => []];
+        $this->assertEqualsCanonicalizing($expected_decks, $decks);
+    }
+    protected function arrangeEmptyCards() {
         $this->mock_cards
         ->expects($this->exactly(2))
         ->method('getCardsInLocation')
         ->with($this->player_id)
         ->willReturn([]);
 
-        $location = '24';
-        $item = [HomeRepository::KEY_LOCATION => $location];
-        $this->mock_items
-        ->expects($this->exactly(1))
-        ->method('getCardsInLocation')
-        ->with($this->player_id)
-        ->willReturn([$item]);
-        $expected_item = $item;
-        // Act
-        $decks = (array) ($this->sut->refresh());
-        // Assert
-        $expected_decks = [Constants::PLANT_NAME => [], Constants::ITEM_NAME => [$expected_item], Constants::ROOM_NAME => []];
-        $this->assertEqualsCanonicalizing($expected_decks, $decks);
+    }
+    protected function getDefaultStoredCard($location) {
+        return [HomeRepository::KEY_LOCATION => $location];
+
+    }
+    protected function getUpdatedCard($card, $element_id) {
+        $updated_card = $card;
+        $updated_card[HomeRepository::KEY_ELEMENT_ID] = $element_id;
+        return $updated_card;
+
     }
 }
 ?>
